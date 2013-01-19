2beb36f273363ebf096168b52b81d6341b51d3f7
Steve Klise <sk@skli.se>
2013-01-18 19:03:06 -0500

    It's alive

---
+++ app.rb
@@ -0,0 +1,19 @@
+require 'bundler'
+Bundler.require
+
+use Rack::Session::Cookie, key: "ooooooohyeaaaaa"
+use OmniAuth::Builder do
+  provider :saml,
+    :assertion_consumer_service_url => "http://localhost:9393/auth/saml/callback",
+    :issuer                         => "ITP Thesis Site",
+    :idp_sso_target_url             => "http://itp.nyu.edu/simplesaml/saml2/idp/SSOService.php",
+    :idp_cert                       => ENV['IDP_CERT']
+end
+
+get '/' do
+  "<a href='/auth/saml'>Sign in with NYU</a>"
+end
+
+post '/auth/saml/callback' do
+  auth = request.env['omniauth.auth']
+end
